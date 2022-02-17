c8b4dd79359f3809d63e41094ef5f5097eea4cd0
siemah <hmi.dayenio@gmail.com>
2022-02-17 02:05:51 +0100

    feat:<InputField />

---
+++ components/input-field/index.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import styles from './style.module.css';
+
+type InputFieldPropsTypes = {
+  name: string;
+  label: string;
+  error?: string | undefined;
+  type?: "text" | "email" | "password" | "textarea";
+  className?: string | undefined;
+  autoFocus?: boolean | undefined;
+  onChange?: React.ChangeEventHandler<HTMLInputElement | HTMLTextAreaElement>;
+}
+export default function InputField({ name, label, type = "text", error, autoFocus, className = '', onChange }: InputFieldPropsTypes) {
+  return (
+    <div className={styles.input__container}>
+      <label className={styles.input__label} htmlFor=''>
+        {label}
+      </label>
+      {
+        type === 'textarea'
+          ? (
+            <textarea
+              name={name}
+              onChange={onChange}
+              className={`${styles.input}${className}`}
+            />
+          )
+          : (
+            <input
+              name={name}
+              type={type}
+              onChange={onChange}
+              className={`${styles.input}${className}`}
+              autoFocus={autoFocus}
+            />
+          )
+      }
+      {
+        error && (
+          <div className={styles.input__error}>
+            {error}
+          </div>
+        )
+      }
+    </div>
+  );
+}
