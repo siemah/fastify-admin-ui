57a7432da68c0f877de10418136f1f4ada41653c
siemah <hmi.dayenio@gmail.com>
2022-02-17 02:12:13 +0100

    feat:<Layout />

---
+++ components/layout/index.tsx
@@ -0,0 +1,21 @@
+import Head from 'next/head';
+import React from 'react';
+import styles from './style.module.css';
+
+type LayoutPropsTypes = {
+  children: React.ReactNode;
+  title: string;
+  description?: string | undefined;
+}
+// todo: add all seo tags here or export it to an external component
+export default function Layout({ title, description, children }: LayoutPropsTypes) {
+  return (
+    <main className={styles.main__block}>
+      <Head>
+        <title>{`${title} - ${process.env.NEXT_PUBLIC_TITLE}`}</title>
+        <meta name='description' content={description || title} />
+      </Head>
+      {children}
+    </main>
+  );
+}
