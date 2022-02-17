ac2fa33e5b27fd5c3c631bfe325098e2b7f605c0
siemah <hmi.dayenio@gmail.com>
2022-02-17 02:12:58 +0100

    conf:rewrite all /api/auth requests 2auth service

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+    return [{
+      source: '/api/:auth*',
+      destination: 'http://localhost:4321/api/:auth*'
+    }];
+  }
 }
 
 module.exports = nextConfig
