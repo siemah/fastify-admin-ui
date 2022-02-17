7aa84dee4c2e1c96ea3397728f35572fa37b56a3
siemah <hmi.dayenio@gmail.com>
2022-02-17 02:38:17 +0100

    feat:<Alert />

---
+++ components/alert/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import styles from './style.module.css';
+
+type AlertPropsTypes = {
+  type: "success" | "error";
+  message: string | null | undefined;
+};
+export default function Alert({ type = 'success', message, }: AlertPropsTypes) {
+  const typeStyle = styles[`alert_${type}`];
+  console.log(typeStyle, type)
+  if (!!message) {
+    return (
+      <div className={`${styles.alert} ${typeStyle}`}>
+        {message}
+      </div>
+    );
+  }
+  return null;
+}
