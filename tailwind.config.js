cdd778bc30acc9b191dd31ac14ca4a73e9b96f33
siemah <hmi.dayenio@gmail.com>
2022-07-23 17:06:37 +0100

    cong:add tailwing

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      aspectRatio: {
+        '4/3': '4 / 3',
+      },
+    },
+  },
+  plugins: [],
+}
